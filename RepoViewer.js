06c4a69853e14b4a775d5afe0111296445f300a2
Vlad Nicula <vladnicula.vn@gmail.com>
2017-11-04 14:35:56 +0200

    Fetch external api

---
+++ RepoViewer.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import fetch from 'fetch'
+
+const fetchMyData = () => {
+  return new Promise((resolve, reject) => {
+    fetch.fetchUrl('https://api.github.com/orgs/facebook/repos', (err, meta, body) => {
+      if ( err ) {
+        return reject(err)
+      }
+      
+      try {
+        const result = JSON.parse(body)
+        return resolve(result)
+      } catch (err) {
+        return reject(err)
+      }
+
+    })
+  })
+}
+
+export default class RepoViewer extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {repos:[]}
+  }
+
+  componentDidMount () {
+    fetchMyData().then((data)=>{
+      this.setState({
+        repos: data
+      })
+    })
+  }
+
+  render () {
+    const { repos } = this.state
+    return (
+      <div className='repo-list-container'>
+        <h2>Repo list</h2>
+        <div className='repo-list'>
+        {repos.map((repo)=>(
+          <div className='repo-list-item' key={repo.id}>
+            <div className='repo-list-item-name'>{repo.full_name}</div>
+            <div className='repo-list-item-stargazers'>{repo.stargazers_count}</div>
+            <div className='repo-list-item-watchers'>{repo.watchers_count}</div>
+            <div className='repo-list-item-forks'>{repo.forks_count}</div>
+          </div>
+        ))}
+        </div>
+      </div>
+    )
+  }
+}
